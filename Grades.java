941c5faab7365e9ebd810e53d30ec33cd3ec3eba
kshah123 <kshahman11@gmail.com>
2016-06-16 12:10:08 -0400

    Add files via upload

--- Grades.java
+++ Grades.java
@@ -15,11 +15,12 @@ public class Grades {
     /**
      * @param args the command line arguments
      */
+    ArrayList<Student> students = new ArrayList();
     public static void main(String[] args) throws FileNotFoundException{
         // TODO code application logic here
         File f = new File("grades.txt");
         Scanner console = new Scanner(f);
-        ArrayList<Student> students = new ArrayList();
+        Grades grades = new Grades();
         ArrayList<String> in = new ArrayList();
         while(console.hasNextLine())
             in.add(console.nextLine());
@@ -28,12 +29,29 @@ public class Grades {
             Student s = new Student(a[0]);
             in.set(i, a[1]);
         }
-        for(int i = 0; i < students.size(); i++){
+        for(int i = 0; i < grades.students.size(); i++){
             String[] c = in.get(i).split(",");
-            double[] d = new double[c.length];
             for(int t = 0; t < c.length; t++)
-                d[t] = Double.parseDouble(c[t]);
+                grades.students.get(i).addGrade(Double.parseDouble(c[t]));
+            grades.students.get(i).setAvg();
         }
+        bubbleSort(grades.students);
     }
-    
+    public static void bubbleSort(ArrayList<Student> ar) {
+               
+               
+        int n = ar.size();
+        Student temp;
+               
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(ar.get(j-1).getAvg() > ar.get(j).getAvg()){
+                temp = ar.get(j-1);
+                ar.set(j-1, ar.get(j));
+                ar.set(j, temp);
+                }                  
+            }
+        }
+       
+    }
 }
