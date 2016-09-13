c4fe38428c7eb0e203685ade4bd7e4dd85d53d90
kshah123 <kshahman11@gmail.com>
2016-09-13 09:12:24 -0400

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+/**
+ *
+ * @author 18shahk
+ */
+public final class q1 {
+    public Comparable[] sSort(Comparable[] x){
+        Comparable[] y = new Comparable[x.length];
+        for(int i = 0; i < x.length; i++)
+            y[i] = x[i];
+        Comparable low;
+        for(int i = 0; i < y.length; i++){
+            for(int j = i; j < y.length; j++){
+                low = y[j];
+            }
+        }
+    }
+}
